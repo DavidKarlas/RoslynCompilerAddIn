aee209e593d9f66bf952ff22f398d0daff4fef53
David Karlaš <david.karlas@xamarin.com>
2015-12-19 13:32:29 +0100

    Enable compiling with Roslyn

--- RoslynCompilerAddIn/ProjectExtension.cs
+++ RoslynCompilerAddIn/ProjectExtension.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.Threading.Tasks;
+using MonoDevelop.Core;
+using MonoDevelop.Projects;
+
 namespace RoslynCompilerAddIn
 {
-	public class ProjectExtension
+	public class ProjectExtension : MonoDevelop.Projects.ProjectExtension
 	{
-		public ProjectExtension()
+		protected override Task<TargetEvaluationResult> OnRunTarget(ProgressMonitor monitor, string target, ConfigurationSelector configuration, TargetEvaluationContext context)
 		{
+			context.GlobalProperties.SetValue("CscToolExe", "csc.exe");
+			context.GlobalProperties.SetValue("CscToolPath", "/Users/davidkarlas/GIT/roslyn/Binaries/Debug/");
+			context.GlobalProperties.SetValue("DebugType", "portable");
+			return base.OnRunTarget(monitor, target, configuration, context);
 		}
 	}
 }

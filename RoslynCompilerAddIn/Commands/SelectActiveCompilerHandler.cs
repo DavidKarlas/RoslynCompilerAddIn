20c38aec38e7d4beb0c8ade296a9e4a7efdb8f01
David Karlaš <david.karlas@xamarin.com>
2015-12-19 13:33:31 +0100

    Added Active Compiler selection in Project menu, so user can easily switch between Default and Roslyn

---
+++ RoslynCompilerAddIn/Commands/SelectActiveCompilerHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+
+namespace RoslynCompilerAddIn.Commands
+{
+	class SelectActiveCompilerHandler : CommandHandler
+	{
+		public static bool IsRoslynCompilerSet = false;
+
+		protected override void Update(CommandArrayInfo info)
+		{
+			if (IdeApp.Workspace.IsOpen) {
+				info.Add("Default", "Default").Checked = !IsRoslynCompilerSet;
+				info.Add("Roslyn", "Roslyn").Checked = IsRoslynCompilerSet;
+			}
+		}
+
+		protected override void Run(object dataItem)
+		{
+			IsRoslynCompilerSet = (dataItem as string) == "Roslyn";
+		}
+	}
+}
+

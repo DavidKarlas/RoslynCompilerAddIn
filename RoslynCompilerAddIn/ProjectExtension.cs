9509a15006fa470ade0c490434eb84ffd8c79825
David Karlaš <david.karlas@xamarin.com>
2015-12-19 13:52:32 +0100

    Bundle Roslyn compiler compiled from 1323a2d2e6e5e9326c6072b9f576ba0b5b8cff27 into AddIn and use it...

--- RoslynCompilerAddIn/ProjectExtension.cs
+++ RoslynCompilerAddIn/ProjectExtension.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using MonoDevelop.Core;
 using MonoDevelop.Projects;
@@ -10,7 +11,7 @@ namespace RoslynCompilerAddIn
 		protected override Task<TargetEvaluationResult> OnRunTarget(ProgressMonitor monitor, string target, ConfigurationSelector configuration, TargetEvaluationContext context)
 		{
 			context.GlobalProperties.SetValue("CscToolExe", "csc.exe");
-			context.GlobalProperties.SetValue("CscToolPath", "/Users/davidkarlas/GIT/roslyn/Binaries/Debug/");
+			context.GlobalProperties.SetValue("CscToolPath", Path.Combine(Path.GetDirectoryName(typeof(ProjectExtension).Assembly.Location), "RoslynCompilerFiles"));
 			context.GlobalProperties.SetValue("DebugType", "portable");
 			return base.OnRunTarget(monitor, target, configuration, context);
 		}
